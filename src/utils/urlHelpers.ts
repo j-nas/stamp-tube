f8d59a2f7e976771250d0b69bf4341b5b1f1e636
Jon Naske <jonnaske@hotmail.com>
2022-10-11 20:26:33 -0700

    added regex to urlextractor

--- src/utils/urlHelpers.ts
+++ src/utils/urlHelpers.ts
@@ -1,12 +1,12 @@
+import z from 'zod'
 
-export default function urlVerifier(videoUrl: string) {
-  const url = videoUrl.trim()
-  const videoId = url.slice(url.indexOf("v=") + 2)
-  if (url.includes("youtube.com/v=") && videoId.length === 11) {
-    return videoId
+export function youtubeIdExtract(url: string) {
+  const reg = /^(https?:)?(\/\/)?((www\.|m\.)?youtube(-nocookie)?\.com\/((watch)?\?(feature=\w*&)?vi?=|embed\/|vi?\/|e\/)|youtu.be\/)([\w\-]{10,20})/i
+  const match = url.match(reg);
+  if (match) {
+
+    return match[9];
+  } else {
+    return null;
   }
-  return null
 }
-
-
-
