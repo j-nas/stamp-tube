2c58829a33d09a1ae8f2cbeeea667a30c0c2aae1
Jon Naske <jonnaske@hotmail.com>
2022-10-12 18:50:26 -0700

    added tailwindcss aspect ratio plugin

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import { signIn, signOut, useSession } from "next-auth/react";
-import { trpc } from "../utils/trpc";
-import Button from "../components/button";
+import type { NextPage } from "next"
+import Head from "next/head"
+import { signIn, signOut, useSession } from "next-auth/react"
+import { trpc } from "../utils/trpc"
+import Button from "../components/button"
+import { useState } from "react"
+import { youtubeIdExtract } from "../utils/urlHelpers"
 const Home: NextPage = () => {
-  const hello = trpc.example.hello.useQuery({ text: "from tRPC" });
+  const [urlForm, setUrlForm] = useState("")
+  const urlValidation = (url: string) => {
+    return youtubeIdExtract(url)
+  }
 
   return (
     <>
@@ -15,6 +20,7 @@ const Home: NextPage = () => {
       </Head>
       <main className="container mx-auto flex min-h-screen flex-col items-center  p-4">
         <h1 className="text-3xl">StampTube</h1>
+        <h2>{urlForm}</h2>
         <p>A place for community submitted timestamps</p>
         <Button>Log in with Google</Button>
         <Button
@@ -23,9 +29,18 @@ const Home: NextPage = () => {
         >
           Log in with Discord
         </Button>
+        <form className="p2 rounded-md border-2 border-zinc-400">
+          <input type="text" onChange={(e) => setUrlForm(e.target.value)} />
+          <button
+            type="submit"
+            className="rounded-md border-2 border-zinc-800 p-2 focus:outline-none"
+          >
+            submit
+          </button>
+        </form>
       </main>
     </>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
