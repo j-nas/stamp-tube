cdd323d1ef3c6784de1e0a078b055793c37f73ea
Jon Naske <jonnaske@hotmail.com>
2022-10-15 10:30:14 -0700

    implemented youtube api info for stamp page

--- src/pages/[stamps].tsx
+++ src/pages/[stamps].tsx
@@ -1,31 +1,49 @@
 import { useRouter } from "next/router"
+import useSwr from "swr"
 import YoutubeEmbed from "../components/youtubeEmbed"
 import Head from "next/head"
+
+const fetcher = async (url: string) => {
+  const res = await fetch(url)
+  if (!res.ok) {
+    const error = new Error(
+      "This is an error constructor. Please give me a bettter message"
+    )
+    throw error
+  }
+  return res.json()
+}
+
 const Stamps = () => {
   const router = useRouter()
   const { v } = router.query
+  const { data, error } = useSwr(`/api/videoInfo?id=${v}`, fetcher)
+
   if (typeof v !== "string") {
     return <p>error</p>
   }
-
   if (!v) {
     return (
       <p className="flex flex-col items-center align-middle">nothing here</p>
     )
   }
+  console.log(data)
+
+  if (error) return <p>Failed to get video info</p>
+  if (!data) return <p>Loading...</p>
 
   return (
     <>
       <Head key="stamp">
-        {" "}
         {/* Youtube API call required to get title from video */}
-        <title>"youtube API call here"</title>
+        <title>{data.title}</title>
       </Head>
       <main>
         <YoutubeEmbed embedId={v} />
         <p className="flex flex-col items-center align-middle">
           Video <a href={`https://www.youtube.com/watch?v=${v}`}>link</a>
         </p>
+        <p>{data.description}</p>
       </main>
     </>
   )
