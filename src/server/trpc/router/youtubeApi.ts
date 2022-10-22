3156ef4ce9740e29755be75496ea29f2a4830018
Jon Naske <jonnaske@hotmail.com>
2022-10-22 13:15:09 -0700

    youtube info fetcher api

--- src/server/trpc/router/youtubeApi.ts
+++ src/server/trpc/router/youtubeApi.ts
@@ -1,18 +1,34 @@
 import { t } from "../trpc"
 import { z } from "zod"
+import axios from "axios"
 
 export const youtubeApiRouter = t.router({
   getVideoInfo: t.procedure
-    .input(z.object({ v: z.string().nullish() }).nullish())
-    .query(({ input }) => {
-      return {
-        video: `youtube video ID is ${input?.v ?? "undefined"}`
-      }
-    })
+    .input(
+      z
+        .object({
+          v: z.union([z.string().length(11).nullish(), z.string().array()]),
+        })
+        .nullish()
+    )
+    .output(
+      z.object({
+        data: z.object({
+          items: z
+            .object({
+              snippet: z.object({
+                title: z.string(),
+                description: z.string(),
+              }),
+            })
+            .array(),
+        }),
+      })
+    )
+
+    .query(async ({ input }) => {
+      const url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&id=${input?.v}&key=${process.env.YOUTUBE_API_KEY}`
+      const { data } = await axios(url)
+      return { data }
+    }),
 })
-
-
-
-
-
-export { }
