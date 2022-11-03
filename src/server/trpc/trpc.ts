6d3848e72ab4686259f0cb60a030a2d24efdf5d5
Jon Naske <jonnaske@hotmail.com>
2022-11-02 17:31:25 -0700

    implemented middleware to check for admin role

--- src/server/trpc/trpc.ts
+++ src/server/trpc/trpc.ts
@@ -1,17 +1,17 @@
-import { initTRPC, TRPCError } from "@trpc/server";
-import type { Context } from "./context";
-import superjson from "superjson";
+import { initTRPC, TRPCError } from "@trpc/server"
+import type { Context } from "./context"
+import superjson from "superjson"
 
 export const t = initTRPC.context<Context>().create({
   transformer: superjson,
   errorFormatter({ shape }) {
-    return shape;
+    return shape
   },
-});
+})
 
 export const authedProcedure = t.procedure.use(({ ctx, next }) => {
   if (!ctx.session || !ctx.session.user) {
-    throw new TRPCError({ code: "UNAUTHORIZED" });
+    throw new TRPCError({ code: "UNAUTHORIZED" })
   }
   return next({
     ctx: {
@@ -19,5 +19,17 @@ export const authedProcedure = t.procedure.use(({ ctx, next }) => {
       // infers that `session` is non-nullable to downstream resolvers
       session: { ...ctx.session, user: ctx.session.user },
     },
-  });
-});
+  })
+})
+
+export const adminProcedure = t.procedure.use(({ ctx, next }) => {
+  if (!ctx.session || ctx.session.user?.role !== "ADMIN") {
+    throw new TRPCError({ code: "FORBIDDEN" })
+  }
+  return next({
+    ctx: {
+      ...ctx,
+      session: { ...ctx.session, user: ctx.session.user },
+    },
+  })
+})
