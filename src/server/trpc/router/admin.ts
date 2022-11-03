51fe556dd8c4f7529ffc24007019934d1ef166f3
Jon Naske <jonnaske@hotmail.com>
2022-11-03 16:39:24 -0700

    implemented changeRole endpoint

--- src/server/trpc/router/admin.ts
+++ src/server/trpc/router/admin.ts
@@ -1,5 +1,5 @@
 import { t, adminProcedure } from "../trpc"
-
+import { z } from "zod"
 export const adminRouter = t.router({
   nukeDatabase: adminProcedure.mutation(({ ctx }) => {
     return () => {
@@ -8,4 +8,22 @@ export const adminRouter = t.router({
       ctx.prisma.user.deleteMany({ where: { role: { equals: "USER" } } })
     }
   }),
+  changeRole: adminProcedure
+    .input(
+      z.object({
+        id: z.string(),
+        role: z.enum(["ADMIN", "USER"]),
+      })
+    )
+    .mutation(({ ctx, input }) => {
+      const { id, role } = input
+      return ctx.prisma.user.update({
+        where: {
+          id: id,
+        },
+        data: {
+          role: role,
+        },
+      })
+    }),
 })
