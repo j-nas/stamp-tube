a2811c462f8e246612facd15e072672eab2edc49
Jon Naske <jonnaske@hotmail.com>
2022-11-04 18:25:58 -0700

    implemented voting infrastructure

---
+++ prisma/migrations/20221104235904_added_vote_table/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "Vote" (
+    "id" TEXT NOT NULL,
+    "upOrDown" SMALLINT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "stampId" TEXT NOT NULL,
+
+    CONSTRAINT "Vote_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Vote" ADD CONSTRAINT "Vote_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Vote" ADD CONSTRAINT "Vote_stampId_fkey" FOREIGN KEY ("stampId") REFERENCES "Stamp"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
